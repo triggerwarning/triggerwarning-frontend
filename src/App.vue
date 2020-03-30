8feffac147733bcf8689e15b1adf8fd191d72503
martin-pi <pharaoh439@gmail.com>
2020-03-29 23:24:29 -0400

    Bought Great Western Font

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,14 @@
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+body {  
   background: #e84545;
+}
+
+#app {
+  font-family: 'Great Western W05 Regular', 'Open Sans', Helvetica, sans-serif;
+  color: #2c3e50;
   min-height: 100vh;
+  overflow: hidden;
 }
 </style>
