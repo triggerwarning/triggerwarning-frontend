d947d29bc1d9c55805c1ef38c8bdeec4f084ec2f
martin-pi <pharaoh439@gmail.com>
2020-03-29 23:26:54 -0400

    Updates to Views, added Game View

---
+++ src/views/Game.vue
@@ -0,0 +1,27 @@
+<template>
+  <default>
+    <div class="fullscreen border border-green-500 w-full">
+      <p5-sketch></p5-sketch>
+    </div>
+  </default>
+</template>
+
+<script>
+import P5Sketch from '@/components/utilities/P5Sketch'
+import Default from '@/views/templates/Default'
+
+export default {
+
+  components: {
+    'default': Default,
+    'p5-sketch': P5Sketch
+  }
+
+}
+</script>
+
+<style>
+  .fullscreen {
+    height: calc(100vh - 48px);
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <default>
-    <img alt="Trigger Warning Gun" src="/gun-corner.svg" style="margin-left: -10vw; margin-top: -5vw; margin-right: 10vw;">
-    
-    <p>Ayy Lmao</p>
-  </default>
+  <no-masthead>
+    <div class="w-full" style="z-index:-10; margin-top:-1vh; margin-left:-1vw;">
+      <img alt="Trigger Warning Gun" src="/gun-corner.svg" class="w-1/2" >
+    </div>
+    <div class="px-2 w-full md:w-1/2 mx-auto mt-20" style=" z-index: 10;">
+      <div class="flex flex-col">
+        <button class="m-1 flex-auto px-8 py-3 rounded-lg bg-white">Create a Room</button>
+        <button class="m-1 flex-auto px-8 py-3 rounded-lg bg-white">Join a Room</button>
+      </div>
+    </div>
+  </no-masthead>
 </template>
 
 <script>
-import Default from '@/views/templates/Default'
+import NoMasthead from '@/views/templates/NoMasthead'
 
 export default {
   name: 'Home',
   components: {
-    'default': Default
+    'no-masthead': NoMasthead
   }
 }
 </script>
