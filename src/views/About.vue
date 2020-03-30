40deff6bdf0e1c5690dc1f17c1ea07c77b84fd8a
martin-pi <pharaoh439@gmail.com>
2020-03-30 19:48:21 -0400

    Added room forms and pacer test

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,8 @@
 <template>
   <default>
-    <div class="container mx-auto mt-12">
-      <h1 class="text-center text-3xl">This is an about page.</h1>
-      <p>We should probably put some rules or something here.</p>
+    <div class="container mx-auto mt-16">
+      <h1 class="text-center text-4xl mb-20">What is this?</h1>
+      <p class="w-3/4 mx-auto">The FitnessGram Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly but gets faster each minute after you hear this signal bodeboop. A sing lap should be completed every time you hear this sound. ding Remember to run in a straight line and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark. Get ready!… Start. ding</p>
     </div>
   </default>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,74 @@
 <template>
-  <no-masthead>
-    <div class="w-full" style="z-index:-10; margin-top:-1vh; margin-left:-1vw;">
-      <img alt="Trigger Warning Gun" src="/gun-corner.svg" class="w-1/2" >
-    </div>
-    <div class="px-2 w-full md:w-1/2 mx-auto mt-20" style=" z-index: 10;">
-      <div class="flex flex-col">
-        <button class="m-1 flex-auto px-8 py-3 rounded-lg bg-white">Create a Room</button>
-        <button class="m-1 flex-auto px-8 py-3 rounded-lg bg-white">Join a Room</button>
+  <default>
+    <div class="px-2 w-full md:w-1/2 lg:w-1/3 mx-auto mt-16" style=" z-index: 10;">
+      <!-- Site Heading -->
+      <img src="gun-full.svg" class="w-1/4 mx-auto">
+      <h1 class="block w-full">
+        <span class="block w-full text-center text-6xl leading-none">Trigger</span>
+        <span class="block w-full text-center text-4xl leading-none">Warning</span>
+      </h1>
+
+      <!-- Anyone can join a room. -->
+      <div class="flex flex-col mt-20 border p-8">
+        <h2 class="block w-full text-center text-3xl leading-none">Join A Room</h2>
+        <label for="roomid" class="leading-none">Room ID:</label>
+        <input name="roomid" type="text" placeholder="A1234" class="input mb-2 p-3 text-center font-bold text-3xl uppercase"> <!--TODO force uppercase output. -->
+        <button class="button my-2 p-3">Join Room</button>
+      </div>
+
+      <!-- If the user is logged in, they can create a new room. -->
+      <div v-if="loggedIn" class="flex flex-col mt-12 border p-8">
+        <h2 class="block w-full text-center text-3xl leading-none">Create A Room</h2>
+        <button class="button my-2 p-3">Create Room</button>
+      </div>
+
+      <!-- Otherwise, tell them to sign in / create an account. -->
+      <div v-else class="flex flex-col mt-12 border p-8">
+        <h2 class="block w-full text-center text-3xl leading-none">Log In to Create Rooms</h2>
+        <button class="button my-2 p-3">Log In</button>
+        <button class="button my-2 p-3">Create Account</button>
       </div>
     </div>
-  </no-masthead>
+  </default>
 </template>
 
 <script>
-import NoMasthead from '@/views/templates/NoMasthead'
+import Default from '@/views/templates/Default'
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      loggedIn: false // TODO Store logged-in status in vuex.
+    }
+  },
+
   components: {
-    'no-masthead': NoMasthead
+    'default': Default
   }
 }
 </script>
+
+<style scoped>
+  .button {
+    background-color: #2b2e4a;
+    color: #ffffff;
+    transition: background-color 0.25s ease;
+  }
+
+  .button:hover {
+    background-color: #576c7d;
+  }
+
+  .input {
+    border: 1px solid #2b2e4a;
+  }
+
+  .input:focus {
+    border: 1px solid #576c7d;
+  }
+
+  .border {
+    border-color: #2b2e4a;
+  }
+</style>
