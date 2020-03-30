3db91ace73ce218c9ff39559e0ded1e37ea2d88c
martin-pi <pharaoh439@gmail.com>
2020-03-29 23:26:34 -0400

    Made the Masthead look like a revolver

--- src/components/Masthead.vue
+++ src/components/Masthead.vue
@@ -1,8 +1,26 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div>
+    <nav class="masthead">
+      <div class="container mx-auto flex">
+        <div class="px-3 py-3 flex-1">
+          <router-link to="/">Trigger Warning</router-link>
+        </div>
+        <div class="px-3 py-3 flex-0">
+          <router-link to="/about">About</router-link>
+        </div>
+        <div class="px-3 py-3 flex-0">
+          <router-link to="/about">Options</router-link>
+        </div>
+        <div class="px-3 py-3 flex-0">
+          <router-link to="/about">Log Out</router-link>
+        </div>
+      </div>
+    </nav>
+    <div class="masthead-decoration">
+      <img src="/gun-masthead.svg" style="width: 160px; transform: translate(-24px, -2px);">
+    </div>
+  
+  </div>
 </template>
 
 <script>
@@ -11,6 +29,18 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.masthead {
+  z-index: 10;
+  max-height: 48px;
+  background-color: #2b2e4a;
+  color: #ffffff;
+}
 
+.masthead-decoration {
+  position: relative;
+  height: 0px;
+  overflow-y: visible;
+  user-select: none;
+}
 </style>
